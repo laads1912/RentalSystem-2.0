5b8a813b10ae758ceb0f6042ae70adc24e402640
Luiz Adriano <santosluiz1912@gmail.com>
2024-10-04 15:40:50 -0300

    Refactor login and registration views

--- controllers/login_controller.py
+++ controllers/login_controller.py
@@ -1,5 +1,6 @@
 from models.user_model import UserModel
 from views.login_view import LoginView
+from controllers.user_controller import UserController
 
 class LoginController:
     def __init__(self):
@@ -11,6 +12,10 @@ class LoginController:
             self.view.show_message("Login Successful", f"Welcome, {email}!")
         else:
             self.view.show_message("Login Failed", "Invalid email or password")
+
+    def handle_open_registration(self):
+        self.view.root.withdraw()
+        UserController(self)
 
     def run(self):
         self.view.mainloop()
