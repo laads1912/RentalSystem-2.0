5061154f8bdbc09b9b4ff1a00eb96b64ecd3d5e0
Josef Bruseke <josefbruseke@gmail.com>
2024-10-07 03:37:44 -0300

    create a user home controller

---
+++ controllers/UserHomeController.py
@@ -0,0 +1,19 @@
+from controllers.RegisteredUsersController import RegisteredUsersController
+from views.UserHomeView import UserHomeView
+
+class UserHomeController:
+    def __init__(self, login_controller):
+        self.login_controller = login_controller
+        self.registered_users_controller = RegisteredUsersController(self)  
+
+    def open_edit_user(self, email):
+        self.registered_users_controller.open_user_edit_page(email)
+        self.view.root.withdraw()
+
+    def open_home_page(self, email):
+        self.view = UserHomeView(self, email)
+        self.view.root.mainloop(email)
+
+    def log_out(self):
+        self.view.root.withdraw()
+        self.login_controller.view.root.deiconify()
