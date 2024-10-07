98aa54a7eeffc4bf272e0df027ed22cee2337e5e
Josef Bruseke <josefbruseke@gmail.com>
2024-10-07 14:08:55 -0300

    unify home_page

--- controllers/EmployeeHomeController.py
+++ controllers/EmployeeHomeController.py
@@ -1,13 +1,18 @@
 from controllers.RegisteredUsersController import RegisteredUsersController
 from views.EmployeeHomeView import EmployeeHomeView
+from views.UserHomeView import UserHomeView
 
 class EmployeeHomeController:
     def __init__(self, login_controller):
         self.login_controller = login_controller
-        self.view = EmployeeHomeView(self)
 
-    def open_home_page(self):
+    def open_employee_home_page(self):
+        self.view = EmployeeHomeView(self)
         self.view.mainloop()
+    
+    def open_user_home_page(self, email):
+        self.view = UserHomeView(self, email)
+        self.view.mainloop(email)
 
     def open_equipments_page(self):
         pass
@@ -20,3 +25,7 @@ class EmployeeHomeController:
         registered_users_controller = RegisteredUsersController(self.login_controller)
         self.view.root.withdraw()
         registered_users_controller.registered_users_page()
+    
+    def open_user_edit_page(self, email):
+        registered_users_controller = RegisteredUsersController(self.login_controller)
+        registered_users_controller.user_edit_page(email)
