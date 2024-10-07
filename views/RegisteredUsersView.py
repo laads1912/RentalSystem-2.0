67ee8e9faf6fd1394888ed467bb5d70160d2127c
Josef Bruseke <josefbruseke@gmail.com>
2024-10-07 12:43:00 -0300

    fix formatation bug

--- views/RegisteredUsersView.py
+++ views/RegisteredUsersView.py
@@ -84,7 +84,7 @@ class RegisteredUsersView:
 
         gender_label = ctk.CTkLabel(
             card_frame,
-            text=f'Gender: {user['gender']}',
+            text=f"Gender: {user['gender']}",
             font=('Poppins Medium', 15),
             text_color='#8f8e8e'
         )
@@ -92,7 +92,7 @@ class RegisteredUsersView:
 
         age_label = ctk.CTkLabel(
             card_frame,
-            text=f'Age: {user['age']}',
+            text=f"Age: {user['age']}",
             font=('Poppins Medium', 15),
             text_color='#8f8e8e'
         )
